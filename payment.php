083d76751590f46f6c21a2a80af22869922f9397
Phoenix <phoenixdev1223@gmail.com>
2023-10-06 21:48:32 +0800

    commit

--- payment.php
+++ payment.php
@@ -11,12 +11,38 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $tokenId = $_POST["tokenId"];
 
     try {
-        $stripe->charges->create([
+        // $stripe->charges->create([
+        //     'amount' => ((int) $amount) * 100,
+        //     'currency' => 'aud',
+        //     'source' => $tokenId,
+        //     'description' => 'Stripe Charge',
+        // ]);
+
+        $paymenMethod = $stripe->paymentMethods->create([
+            'type' => 'card',
+            'card' => [
+                'token' => $tokenId
+            ],
+            'billing_details' => [
+                'email' => $email,
+                'name' => $name
+            ]
+        ]);
+
+        $paymentIntent = $stripe->paymentIntents->create([
             'amount' => ((int) $amount) * 100,
             'currency' => 'aud',
-            'source' => $tokenId,
-            'description' => 'Stripe Charge',
+            'payment_method_types' => ['card'],
+            'payment_method' => $paymenMethod['id'],
+            'capture_method' => 'automatic',
         ]);
+
+        $stripe->paymentIntents->confirm(
+            $paymentIntent['id'],
+            [
+                'payment_method' => $paymenMethod['id'],
+            ]
+        );
 
         echo "Success!!!";
     } catch (Exception $e) {
